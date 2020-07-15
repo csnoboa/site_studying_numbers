af3325c9cda7ec4e3f3d27fc7ed0d1b1063b25ac
csnoboa <caique2noboa@gmail.com>
2020-07-15 09:31:45 -0300

    ads teste03

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -783,8 +783,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7847a20dd8ab2c2ac874cc6e0a63a1e2",
-"/": "7847a20dd8ab2c2ac874cc6e0a63a1e2",
+"index.html": "2689ebfae13bc6351798ad99953fd374",
+"/": "2689ebfae13bc6351798ad99953fd374",
 "main.dart.js": "7227ca0180092d00856ecc04b1c44a08",
 "manifest.json": "ea83346d947cbd83a9922ecec0f318de"
 };
